8132ce99deec07cd26b4490f158e5d233a98ad3c
Gerson Clara <gersonfc15@hotmail.com>
2024-04-04 20:53:54 -0300

    Alteracao Funcao Mult e Div

--- calculator.js
+++ calculator.js
@@ -1,25 +1,30 @@
-let v1 = []
-let v2 = []
-
 // retorna o valor da soma entre os dois parâmetros
-
-
 function sum(v1, v2) {
     return v1 + v2;
 }
 
 // retorna o valor da subtração entre os dois parâmetros
-
 function sub(v1, v2) {
   return v1 - v2;
 }
 
-
 // retorna o valor da divisão entre os dois parâmetros
-function div(v1, v2) {}
+function div(v1, v2) {
+    let resultado = v1/v2
+
+    return resultado
+}
 
 // retorna o valor da multiplicação entre os dois parâmetros
-function mult(v1, v2) {}
+function mult(v1, v2) {
+    let resultado = 0
+
+    for (let i = 0; i < v2; i++) {
+        resultado += v1
+    }
+
+    return resultado
+}
 
 // retorna o valor da raiz quadrada do valor recebido por parâmetro
 function square(v1) {}
