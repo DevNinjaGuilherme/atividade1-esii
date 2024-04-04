686e03ed05b0a787fa433652bb636fc305d446ce
João Vitor DOlinski <joaovitordolinski@gmail.com>
2024-04-04 20:17:53 -0300

    Alteracao Funcao Soma

--- calculator.js
+++ calculator.js
@@ -1,8 +1,19 @@
+let v1 = []
+let v2 = []
+
 // retorna o valor da soma entre os dois parâmetros
-function sum(v1, v2) {}
+
+
+function sum(v1, v2) {
+    return v1 + v2;
+}
 
 // retorna o valor da subtração entre os dois parâmetros
-function sub(v1, v2) {}
+
+function sub(v1, v2) {
+  return v1 - v2;
+}
+
 
 // retorna o valor da divisão entre os dois parâmetros
 function div(v1, v2) {}
@@ -13,4 +24,15 @@ function mult(v1, v2) {}
 // retorna o valor da raiz quadrada do valor recebido por parâmetro
 function square(v1) {}
 
-console.log(sum(v1, v2));
+
+// Retorno das Funções
+
+console.log(sum(8,2));
+
+console.log(sub(4,2));
+
+console.log(div(6,2));
+
+console.log(mult(4,2));
+
+console.log(square(4));
